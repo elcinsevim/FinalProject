a1a785334f21cb18bf91ee42c3a113f38cd7a9c7
Elçin SEVİM <46248880+elcinsevim@users.noreply.github.com>
2021-03-02 13:56:40 +0300

    02.03.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,18 +15,18 @@ namespace ConsoleUI
 
         }
 
-        private static void CategoryTest()
-        {
-            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
-            {
-                Console.WriteLine(category.CategoryName);
-            }
-        }
+        //private static void CategoryTest()
+        //{
+        //    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        //    foreach (var category in categoryManager.GetAll())
+        //    {
+        //        Console.WriteLine(category.CategoryName);
+        //    }
+        //}
 
         private static void ProductTest()
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal(),new CategoryManager(new EfCategoryDal()));
             var result = productManager.GetProductDetails();
             if (result.Success == true)
             {
